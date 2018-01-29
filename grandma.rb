03680a58b73edb5583aac4d2ea0213faf87580d5
Brendan O'Dea <brendan.ode4@gmail.com>
2018-01-29 17:46:01 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(say)
 
   if say == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif say == say.upcase
     "NO, NOT SINCE 1938!"
   else
